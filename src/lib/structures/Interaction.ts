d5312d5eff7b031ff46a79e7f5d1d00859c63a12
gc <30398469+gc@users.noreply.github.com>
2021-10-26 00:35:12 +1100

    move interaction props to root class

--- src/lib/structures/Interaction.ts
+++ src/lib/structures/Interaction.ts
@@ -1,9 +1,27 @@
-import type { APIInteraction } from 'discord-api-types/v9';
+import type { APIInteraction, APIInteractionGuildMember, APIMessage, APIUser, Snowflake } from 'discord-api-types/v9';
 
 export class Interaction<T extends APIInteraction> {
 	interaction: T;
+	id: Snowflake;
+	applicationID: Snowflake;
+	token: string;
+
+	message?: APIMessage;
+	channelID?: Snowflake;
+	guildID?: Snowflake;
+	member?: APIInteractionGuildMember;
+	user?: APIUser;
 
 	constructor(interaction: T) {
 		this.interaction = interaction;
+
+		this.id = interaction.id;
+		this.guildID = interaction.guild_id;
+		this.channelID = interaction.channel_id;
+		this.applicationID = interaction.application_id;
+		this.member = interaction.member;
+		this.user = interaction.user;
+		this.token = interaction.token;
+		this.message = interaction.message;
 	}
 }
